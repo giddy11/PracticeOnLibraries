5279fdc6a14cf99c933daeb6df639f937aabd046
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-05-23 17:08:09 +0100

    math ui

---
+++ MathLibrary/ArithmeticOperations.cs
@@ -0,0 +1,28 @@
+﻿namespace MathLibrary;
+
+public class ArithmeticOperations
+{
+    public const float PI = 3.1415f;
+
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    
+    public int Substract(int a, int b)
+    {
+        return a - b;
+    }
+    
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+
+}
