53ea09b7ee70e0288d168bf6672f4193c1474c51
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-05-23 12:46:48 +0100

    completed

---
+++ ClassLibrary1/PersonProcessor.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoLibrary;
+
+public class PersonProcessor
+{
+    public static string JoinName(string firstName, string lastName)
+    {
+        return firstName + " " + lastName;
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using DemoLibrary;
+
+
+string fName, lName, fullName;
+
+Console.Write("What is your First name: ");
+fName = Console.ReadLine();
+
+Console.Write("What is your Second name: ");
+lName = Console.ReadLine();
+
+fullName = PersonProcessor.JoinName(fName, lName);
+
+Console.WriteLine($"Your full name is {fullName}.");
