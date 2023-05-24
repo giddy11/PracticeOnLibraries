fc0a2e97e9216472d5f140c8c07fc2514f3a1c9e
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-05-24 12:35:07 +0100

    banksystem library

---
+++ BankyStuffLibrary/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace BankyStuffLibrary;
+
+public class Transaction
+{
+    public Transaction(decimal amount, DateTime date, string notes)
+    {
+        Amount = amount;
+        Date = date;
+        Notes = notes;
+    }
+
+    public decimal Amount { get; }
+    public DateTime Date { get; }
+    public string Notes { get; }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using BankyStuffLibrary;
+
+
+var accounts = new List<BankAccount>
+{
+    new BankAccount("Gideon", 10000),
+    new BankAccount("Daniel", 30000),
+    new BankAccount("Henry", 15000)
+};
+
+foreach (var item in accounts)
+{
+    Console.WriteLine($"Account {item.AcctNumber} was created for {item.Owner} with balance of {item.Balance}.");
+}
+
+Console.WriteLine("Below are updated transactions");
+
+accounts[0].MakeWithdrawal(150, DateTime.Now, "Chicken");
+accounts[1].MakeDeposit(-300, DateTime.Now, "stealing");
+
+
+foreach (var item in accounts)
+{
+    Console.WriteLine($"Account {item.AcctNumber} was created for {item.Owner} with balance of {item.Balance}.");
+}
