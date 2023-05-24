cd145cea8ef3365ebf2b72e2522012c3864cc8d9
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-05-24 09:31:52 +0100

    implemented the calculatorUI

--- CaculatorUI/MainWindow.xaml.cs
+++ CaculatorUI/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MathLibrary;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CaculatorUI
 {
@@ -23,6 +11,45 @@ namespace CaculatorUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// The int.parse converts the string value to an interger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Solve_Click(object sender, RoutedEventArgs e)
+        {
+            int operand1 = int.Parse(txtBox1.Text);
+            int operand2 = int.Parse(txtBox3.Text);
+            char operation = txtBox2.Text.ToCharArray()[0];
+
+            int result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = ArithmeticOperations.Add(operand1, operand2);
+                    break;
+
+                case '-':
+                    result = ArithmeticOperations.Substract(operand1, operand2);
+                    break;
+
+                case '*':
+                    result = ArithmeticOperations.Multiply(operand1, operand2);
+                    break;
+
+                case '/':
+                    result = ArithmeticOperations.Divide(operand1, operand2);
+                    break;
+
+                default:
+                    MessageBox.Show("Invalid operation");
+                    break;
+            }
+
+            txtBox4.Text = result.ToString();
         }
     }
 }
--- MathLibrary/ArithmeticOperations.cs
+++ MathLibrary/ArithmeticOperations.cs
@@ -4,22 +4,22 @@ public class ArithmeticOperations
 {
     public const float PI = 3.1415f;
 
-    public int Add(int a, int b)
+    public static int Add(int a, int b)
     {
         return a + b;
     }
     
-    public int Substract(int a, int b)
+    public static int Substract(int a, int b)
     {
         return a - b;
     }
     
-    public int Multiply(int a, int b)
+    public static int Multiply(int a, int b)
     {
         return a * b;
     }
 
-    public int Divide(int a, int b)
+    public static int Divide(int a, int b)
     {
         return a / b;
     }
