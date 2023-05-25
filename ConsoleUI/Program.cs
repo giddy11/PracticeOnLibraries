75337b8c43182ad78e610a8a9f2bbe76717aa74d
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-05-25 16:48:15 +0100

    completed the implementation

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,45 @@
-﻿using DemoLibrary;
+﻿using TempConversionLibrary;
+
+char fromScale, toScale;
+
+while (true)
+{
+    Console.WriteLine("What temperature do you want to convert? (int number please)");
+    int fromTemp = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"What temperature scale is {fromTemp} in? (C F or K)");
+    fromScale = Console.ReadLine()[0];
+
+    Console.WriteLine("What temperature do you want to convert to? (C F or K)");
+    toScale = Console.ReadLine()[0];
+
+    var calculator = new ConversionCalculator(fromTemp, fromScale);
 
 
-string? fName, lName, fullName;
+    switch(toScale)
+    {
+        case 'C':
+            //res = calculator.ToCelsius();
+            Console.WriteLine($"{fromTemp}°{fromScale} is {calculator.ToCelsius()}°{toScale}\n\n");
+            //Console.WriteLine($"{fromTemp} in Celsius is {calculator.ToCelsius()}\n\n");
+            break;
 
-Console.Write("What is your First name: ");
-fName = Console.ReadLine();
+        case 'F':
+            //res = calculator.ToFahrenheight();
+            Console.WriteLine($"{fromTemp}°{fromScale} is {calculator.ToFahrenheight()}°{toScale}\n\n");
+            //Console.WriteLine($"{fromTemp} in Celsius is {calculator.ToFahrenheight()}\n\n");
+            break;
 
-Console.Write("What is your Second name: ");
-lName = Console.ReadLine();
+        case 'K':
+            //res = calculator.ToKelvin();
+            Console.WriteLine($"{fromTemp}°{fromScale} is {calculator.ToKelvin()}°{toScale}\n\n");
+            //Console.WriteLine($"{fromTemp} in Celsius is {calculator.ToKelvin()}\n\n");
+            break;
+    }
 
-fullName = PersonProcessor.JoinName(fName, lName);
+   
 
-Console.WriteLine($"Your full name is {fullName}.");
+}
+
+
+
---
+++ TempConversionLibrary/ConversionCalculator.cs
@@ -0,0 +1,85 @@
+﻿namespace TempConversionLibrary;
+
+public class ConversionCalculator
+{
+    public ConversionCalculator(float baseTemp, char baseScaleName)
+    {
+        BaseTemp = baseTemp;
+        BaseScaleName = baseScaleName;
+    }
+
+    public float ToFahrenheight()
+    {
+        float computedTemp = -10000;
+        switch(BaseScaleName)
+        {
+            case 'F':
+                computedTemp = BaseTemp;
+                break;
+
+            case 'C':
+                computedTemp = BaseTemp * 9 / 5 + 32;
+                break;
+
+            case 'K':
+                computedTemp = (BaseTemp - 273) * 9 / 5 + 32;
+                break;
+        }
+        return computedTemp;
+    }
+
+    public float ToCelsius()
+    {
+        float computedTemp = -10000;
+        switch (BaseScaleName)
+        {
+            case 'C':
+                computedTemp = BaseTemp;
+                break;
+
+            case 'F':
+                computedTemp = (BaseTemp - 32) * 5 / 9;
+                break;
+
+            case 'K':
+                computedTemp = BaseTemp - 273;
+                break;
+        }
+        return computedTemp;
+    }
+
+    public float ToKelvin()
+    {
+        float computedTemp = -10000;
+        switch (BaseScaleName)
+        {
+            case 'K':
+                computedTemp = BaseTemp;
+                break;
+
+            case 'C':
+                computedTemp = BaseTemp + 273;
+                break;
+
+            case 'F':
+                computedTemp = (BaseTemp - 32) * 5 / 9 + 273;
+                break;
+        }
+        return computedTemp;
+    }
+
+
+    public float BaseTemp { get; set; }
+    public char BaseScaleName { get; set; }
+
+
+    /*
+     This placeholder initialization is commonly used in programming as a convention to handle exceptional 
+    or error conditions. It serves as a reminder to handle cases where the expected value is not assigned
+    or computed correctly.
+
+
+
+     */
+
+}
